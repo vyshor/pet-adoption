94a429cef033887b90fb00e3d55b58993580f803
vyshor <vyshornehc@gmail.com>
2020-07-21 20:55:22 +0800

    Implemented cloudstorage for the images

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from flask import Flask, render_template, redirect, url_for, request, abort, Res
 from flask_mail import Mail, Message
 from db_operations import *
 from forms import AdoptionForm, CreateListingForm
+from gcloudstorage import upload_blob
 
 
 app = Flask(__name__)
@@ -53,13 +54,21 @@ def root():
         if form.validate_on_submit():
             print(form.email)
             f = form.upload_img.data
+
             print(f)
             # TODO: Save the img file into Firebase (?)
         print(listings)
         return render_template('main.html', listings=listings, form=form)
     elif request.method == 'POST':
+        for key, upload in request.files.items():
+            identity = str(uuid.uuid4())  # or uuid.uuid4().hex
+            print(identity)
+            try:
+                img_url = upload_blob(request.files[key], identity, content_type=upload.content_type)
+                print(img_url)
+            except:
+                pass
         form_dict = request.form.to_dict()
-        print(form_dict)
         try:
             new_listing = createListingWithoutId(
                 form_dict['pet_name'],
@@ -67,10 +76,9 @@ def root():
                 form_dict['breed'],
                 form_dict['dob'],
                 form_dict['description_of_pet'],
-                'img_url',
+                img_url,
                 form_dict['email'],
             )
-            print(new_listing.listing_id)
             create_listing(new_listing)
         except:
             pass
